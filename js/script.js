45151c537a411cac51a7a6c6799e3e81990af50d
Melanie Almeida <almeida.melanie@gmail.com>
2020-11-27 11:46:55 +0100

    almost done

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 // global variables
 let employees = [];
 const urlAPI = `https://randomuser.me/api/?results=12&inc=name,picture,
-email,location,phone,dob &noinfo &nat=US`
+email,location,phone,dob &noinfo &nat=US`;
 const gridContainer = document.querySelector(".grid-container");
 const overlay = document.querySelector(".overlay");
 const modalContainer = document.querySelector(".modal-content");
@@ -12,7 +12,7 @@ fetch(urlAPI)
   .then(res => res.json())
   .then(res => res.results)
   .then(displayEmployees)
-  .catch(err => console.log(err))
+  .catch(err => console.log(err));
 
 
   function displayEmployees(employeeData){
@@ -36,28 +36,28 @@ employees.forEach((employee,index)=>{
       <p class="address">${city}</p>
     </div>
     </div>
-  `
+  `;
 });
 
 gridContainer.innerHTML=employeeHTML;
 }
 
 function displayModal(index){
-  let {nae,dob,phone, email,location:{city,street,state,postcode},picture}=employees[index];
+  let {name,dob,phone, email,location:{city,street,state,postcode},picture}=employees[index];
 
   let date=new Date(dob.date);
 
   const modalHTML=
   `<img class="avatar" src="${picture.large}" />
-  <div class="text-container">
-  <h2 class="name">${name.first} ${name.last}</h2>
-  <p class="email">${email}</p>
-  <p class="address">${city}</p>
-  <hr />
-  <p>${phone}</p>
-  <p class="address">${street}, ${state} ${postcode}</p>
-  <p>Birthday:
-  ${date.getMonth()}/${date.getDate()}/${date.getFullYear()}</p>
+    <div class="text-container">
+    <h2 class="name">${name.first} ${name.last}</h2>
+    <p class="email">${email}</p>
+    <p class="address">${city}</p>
+    <hr />
+    <p>${phone}</p>
+    <p class="address">${street}, ${state} ${postcode}</p>
+    <p>Birthday:
+    ${date.getMonth()}/${date.getDate()}/${date.getFullYear()}</p>
   </div>
   `;
   overlay.classList.remove("hidden");
@@ -67,8 +67,7 @@ function displayModal(index){
   gridContainer.addEventListener('click', e => {
 // make sure the click is not on the gridContainer itself
 if (e.target !== gridContainer) {
-// select the card element based on its proximity to actual element
-clicked
+// select the card element based on its proximity to actual element clicked;
 const card = e.target.closest(".card");
 const index = card.getAttribute('data-index');
 displayModal(index);
