2e4f1fd09776ea3d6630d1b0b8e7be0e30b9603d
Melanie Almeida <almeida.melanie@gmail.com>
2020-12-04 10:36:11 +0100

    commenting

--- js/script.js
+++ js/script.js
@@ -1,25 +1,23 @@
 // global variables
-let employees = [];
-const urlAPI = `https://randomuser.me/api/?results=12&inc=name,picture,
-email,location,phone,dob &noinfo &nat=US`;
-const gridContainer = document.querySelector(".grid-container");
-const overlay = document.querySelector(".overlay");
-const modalContainer = document.querySelector(".modal-content");
-const modalClose = document.querySelector(".modal-close");
+let employees=[]; //empty array that will hold values from the API
+const urlAPI= `https://randomuser.me/api/?results=12&inc=name,picture,
+email,location,phone,dob &noinfo &nat=US`; //string literal that stores the url of the API, complete with desired options
+ const gridContainer=document.querySelector('.grid-container');//stores the DOM element that is the container for the employees
+ const overlay=document.querySelector('.overlay');//stores the DOM element that acts as an overlay for the modal.
+ const modalContainer=document.querySelector('.modal-content');//stores the DOM element that is the modal's close button
+ const modalClose=document.querySelector('.modal-close');//stores the DOM element that is the modal's close button.
 
 //fetch data from api
-fetch(urlAPI)
-  .then(res => res.json())
-  .then(res => res.results)
-  .then(displayEmployees)
-  .catch(err => console.log(err));
+fetch(urlAPI) //pass the url information to fetch
+.then(res=>res.json()) //format the response as JSON
+.then(res=>res.results) //return the results of the response
+.then(displayEmployees)//pass control to the displayEmployees function
+.catch(err=>console.log(err))//to display the response and catch errors using Chrome dev tools
 
 
   function displayEmployees(employeeData){
-    employees=employeeData;
-
-    //store the employee HTML as we create it
-  let employeeHTML='';
+    employees=employeeData; //by setting the employees variable equal to employeeData, it can be accessed outside of the function
+  let employeeHTML=''; //this variable will contain the HTML markup for the employee elements.
   //loop through each employee and create HTML markup
 employees.forEach((employee,index)=>{
   let name=employee.name;
@@ -37,15 +35,15 @@ employees.forEach((employee,index)=>{
     </div>
     </div>
   `;
-});
+}); // using template liretal, add each employee markup to the employeeHTML
 
 gridContainer.innerHTML=employeeHTML;
 }
 
-function displayModal(index){
-  let {name,dob,phone, email,location:{city,street,state,postcode},picture}=employees[index];
+function displayModal(index){ //function scoped variables for the information that needs to be displayed
+  let {name,dob,phone, email,location:{city,street,state,postcode},picture}=employees[index]; //using the information from the employees array
 
-  let date=new Date(dob.date);
+  let date=new Date(dob.date); //variavle thatcreates a new Date Object based on the employee's date of birth
 
   const modalHTML=
   `<img class="avatar" src="${picture.large}"/>
